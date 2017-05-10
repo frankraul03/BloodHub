90329636a538d15ab31f328acd0ef4bdd3e02106
rp <frankraul03@gmail.com>
2017-05-10 11:45:27 +0545

    Added an Activity Main file Added a method

--- BloodHub/app/src/main/java/com/example/rp/bloodhub/MainActivity.java
+++ BloodHub/app/src/main/java/com/example/rp/bloodhub/MainActivity.java
@@ -3,11 +3,22 @@ package com.example.rp.bloodhub;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.sql.SQLOutput;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public float multiply(){
+        int i=1;
+        int j=2;
+        int k= j+i;
+        System.out.println(k);
+        return 0;
+
     }
 }
